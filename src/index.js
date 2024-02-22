08c265a944d542a29eb07626ae87e838b612e20e
joydumply <nick.babichev@gmail.com>
2024-02-22 20:37:57 +0200

    developed reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-const initialState = 0;
+const reducer = (state = 0, action) => {
+	switch (action.type) {
+		case 'INC':
+			return state + 1;
 
-const reducer = (state, action) => {
-	if (action.type === 'INC') {
-		return state + 1;
+		default:
+			return state;
 	}
-	return 0;
 };
 
-let state = reducer(initialState, { type: 'INC' });
+let state = reducer(undefined, { type: 'INC' });
 
+state = reducer(state, { type: 'INC' });
 console.log(state);
